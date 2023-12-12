5bfc1641cc633464308b5c08406118a32fe688e8
OlegMorev <proks200413@gmail.com>
2023-12-12 05:08:44 +0300

    sofa rotation changed

--- Flying Sofa Simulator (Morev)/Assets/Scripts/Movement/SofaMovement.cs
+++ Flying Sofa Simulator (Morev)/Assets/Scripts/Movement/SofaMovement.cs
@@ -5,34 +5,19 @@ using UnityEngine;
 
 public class SofaMovement : MonoBehaviour
 {
+    [SerializeField] GameObject _sofaModelObject;
     [SerializeField] int _movingSpeed;
     [SerializeField] int _rotationSpeed;
     [SerializeField] int _pitchSoftness;
-    [Header("Test values")]
-    [Range(0f, 1f)]
-    [SerializeField] float turnLeft;
-    [Range(0f, 1f)]
-    [SerializeField] float turnRight;
-    [SerializeField] Vector2 movementStick;
-    [Range(0f, 1f)]
-    [SerializeField] float goUp;
-    [Range(0f, 1f)]
-    [SerializeField] float goDown;
     void Start()
     {
         StartCoroutine(AngleChanger());
-        //StartCoroutine(TestAngleChanger());
     }
     void FixedUpdate()
     {
         RotateInChosenDirection();
         MoveInChosenDirection();
         ChangeHigh();
-
-
-        //TestRotateInChosenDirection();
-        //TestMoveInChosenDirection();
-        //TestChangeHigh();
     }
     void RotateInChosenDirection()
     {
@@ -40,15 +25,6 @@ public class SofaMovement : MonoBehaviour
         {
             Vector3 currentRotationValue = transform.eulerAngles;
             currentRotationValue.y += (ControllerInputValues.rightTrigger - ControllerInputValues.leftTrigger)*Time.deltaTime * _rotationSpeed;
-            transform.eulerAngles = currentRotationValue;
-        }
-    }
-    void TestRotateInChosenDirection()
-    {
-        if (turnLeft != 0 || turnRight != 0)
-        {
-            Vector3 currentRotationValue = transform.eulerAngles;
-            currentRotationValue.y += (turnRight - turnLeft)*Time.deltaTime * _rotationSpeed;
             transform.eulerAngles = currentRotationValue;
         }
     }
@@ -60,29 +36,12 @@ public class SofaMovement : MonoBehaviour
             transform.position = transform.position + new Vector3(transform.right.x, 0, transform.right.z) * Time.deltaTime * ControllerInputValues.leftStickValue.x * _movingSpeed;
         }
     }
-    void TestMoveInChosenDirection()
-    {
-        if (movementStick != Vector2.zero)
-        {
-            transform.position = transform.position + new Vector3(transform.forward.x,0, transform.forward.z) * Time.deltaTime * movementStick.y * _movingSpeed;
-            transform.position = transform.position + new Vector3(transform.right.x,0, transform.right.z) * Time.deltaTime * movementStick.x * _movingSpeed;
-        }
-    }
     void ChangeHigh()
     {
         if(ControllerInputValues.rightStickValue.y != 0)
         {
             Vector3 pos = transform.position;
             pos.y += ControllerInputValues.rightStickValue.y * Time.deltaTime;
-            transform.position = pos;
-        }
-    }
-    void TestChangeHigh()
-    {
-        if (goUp != 0 || goDown != 0)
-        {
-            Vector3 pos = transform.position;
-            pos.y += (goUp - goDown) * Time.deltaTime;
             transform.position = pos;
         }
     }
@@ -116,39 +75,6 @@ public class SofaMovement : MonoBehaviour
         }
         return z;
     }
-
-    float TestGetXPitch()
-    {
-        float x = 0;
-        x += movementStick.x * 20;
-        x -= (goUp - goDown) * 10;
-        if (x > 30)
-        {
-            x = 30;
-        }
-        if (x < -30)
-        {
-            x = -30;
-        }
-        return x;
-    }
-    float TestGetZPitch()
-    {
-        float z = 0;
-        z -= movementStick.y * 20;
-        z -= (turnRight - turnLeft) * 10;
-        if (z > 30)
-        {
-            z = 30;
-        }
-        if (z < -30)
-        {
-            z = -30;
-        }
-        return z;
-    }
-
-
     IEnumerator AngleChanger()
     {
         Vector2 currentAngles = new Vector2(0, 0);
@@ -160,22 +86,7 @@ public class SofaMovement : MonoBehaviour
             {
                 currentAngles -= (currentAngles - neededAngles) / _pitchSoftness;
             }
-            transform.eulerAngles = new Vector3(currentAngles.x, transform.eulerAngles.y, currentAngles.y);
-            yield return null;
-        }
-    }
-    IEnumerator TestAngleChanger()
-    {
-        Vector2 currentAngles = new Vector2(0, 0);
-        Vector2 neededAngles = new Vector2(TestGetXPitch(), TestGetZPitch());
-        while (true)
-        {
-            neededAngles = new Vector2(TestGetXPitch(), TestGetZPitch());
-            if (currentAngles != neededAngles)
-            {
-                currentAngles -= (currentAngles - neededAngles) / _pitchSoftness;
-            }
-            transform.eulerAngles = new Vector3(currentAngles.x, transform.eulerAngles.y, currentAngles.y);
+            _sofaModelObject.transform.localEulerAngles = new Vector3(currentAngles.x, _sofaModelObject.transform.localEulerAngles.y, currentAngles.y);
             yield return null;
         }
     }
