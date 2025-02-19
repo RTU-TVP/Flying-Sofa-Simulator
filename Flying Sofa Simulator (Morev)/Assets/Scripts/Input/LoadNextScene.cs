87eb1ea7ef778645b426a541ff8ca7b1ffc1e7ec
ShutovKS <schutow.kir@yandex.ru>
2025-02-19 22:52:51 +0300

    Add bootstrap

---
+++ Flying Sofa Simulator (Morev)/Assets/Scripts/Input/LoadNextScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
