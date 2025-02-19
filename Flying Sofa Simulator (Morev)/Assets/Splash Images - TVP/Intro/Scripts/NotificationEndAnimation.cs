fae247e00a478dd04bde5384f74ee6bd3119b54e
ShutovKS <schutow.kir@yandex.ru>
2025-02-19 22:52:18 +0300

    Add intro

---
+++ Flying Sofa Simulator (Morev)/Assets/Splash Images - TVP/Intro/Scripts/NotificationEndAnimation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace SplashImages.Intro
+{
+    public class NotificationEndAnimation : MonoBehaviour
+    {
+        public event UnityAction OnEndAnimation;
+
+        public void EndAnimation()
+        {
+            OnEndAnimation?.Invoke();
+        }
+    }
+}
